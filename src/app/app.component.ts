7575f79c1ea4a1eac2fb8d423cbf95e42d686184
Mark Percy <mark.percy98@outlook.com>
2023-12-22 17:09:11 +0000

    added festive snow fall for during december

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'presentTracker';
+  date = new Date()
+  ngOnInit(): void {
+    if(this.date.getMonth() == 11) this.createSnowflakes();
+  }
+
+  createSnowflakes() {
+    const snowflakesContainer = document.querySelector('.snowfall-background');
+    for (let i = 0; i < 100; i++) {
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake';
+      snowflake.style.left = Math.random() * 100 + 'vw';
+      snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
+      snowflake.style.opacity = Math.random().toString();
+      snowflake.style.width = Math.random() * 10 + 5 + 'px';
+      snowflake.style.height = snowflake.style.width;
+
+      snowflakesContainer?.appendChild(snowflake);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
